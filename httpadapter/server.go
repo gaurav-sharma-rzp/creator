e62fe285f3f22fc0f9d51a3c5ddd663e6629586e
gaurav-sharma-rzp <gaurav.sharma@razorpay.com>
2021-04-21 23:22:23 +0530

    adds http server adapter

---
+++ httpadapter/server.go
@@ -0,0 +1,20 @@
+package httpadapter
+
+import (
+	"github.com/gorilla/mux"
+)
+
+type server struct {
+	Router *mux.Router
+}
+
+func NewServer(router *mux.Router) *server {
+	return &server{
+		Router: router,
+	}
+}
+
+func (s *server) RegisterRoutes() {
+	s.addHealthRoutes()
+	s.addFloraRoutes()
+}
